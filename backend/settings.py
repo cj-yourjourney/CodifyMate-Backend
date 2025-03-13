9aeba314a8fc5eff7e20504b5d1d7f10c6f281da
cj <49462194+cj-yourjourney@users.noreply.github.com>
2025-03-13 12:07:01 -0700

    added back EC2 IP

--- backend/settings.py
+++ backend/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = "django-insecure-$vkoy%iofb(wmcw20nt&ou&uui5$h81=s*^m3doix+^mq2nc0m
 DEBUG = True
 
 # Allow
-ALLOWED_HOSTS = ["127.0.0.1", "localhost"]
+ALLOWED_HOSTS = ["127.0.0.1", "localhost", "54.219.134.58"]
 
 
 # Application definition
